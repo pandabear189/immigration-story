23f0f042a810908780c2d51d25718a6044c726bf
Eric <88685021+pandabear189@users.noreply.github.com>
2021-11-16 10:12:19 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const typed = new Typed(".typing", {
-    strings: ["Fun", "Engaging", "Exciting", "New"],
+    strings: ["Immigration Story", "My Immigration Story"],
     loop: true,
     typeSpeed: 80,
     backSpeed: 30,
